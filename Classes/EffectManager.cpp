3cb2800571b82f21b25a754420e92738a5271fcf
abiles <pgalpga@hanmail.net>
2014-10-09 02:34:56 +0900

    EffectManager

--- Classes/EffectManager.cpp
+++ Classes/EffectManager.cpp
@@ -107,18 +107,32 @@ void EffectManager::useEffectSelectedSizeByUser(EffectType selectedEffect, cocos
 void EffectManager::useEffectLinearMissileCollision(cocos2d::Rect obRect, int effectPlayNum, Point AnchorPoint)
 {
 	float needEffectScale = 0.5;
+	
+	//m_MainSprite = GET_RESOURCE_MANAGER()->createAnimation("ETLinearMissileCollision1.png");
+	//m_MainAnimation = GET_RESOURCE_MANAGER()->createAnimation("ETLinearMissileCollision%d.png", 1, 9, 0.1f);
 
-	//현욱이가 만들어 주면 여기 해제
-	m_MainSprite = GET_RESOURCE_MANAGER()->createAnimation("ETLinearMissileCollision1.png");
-	m_MainAnimation = GET_RESOURCE_MANAGER()->createAnimation("ETLinearMissileCollision%d.png", 1, 9, 0.1f);
-
-	//이게 좀 앞에서 Rect값을 이용해서 터지면 좋겠다. 
 	float ratioX = obRect.size.width / m_MainSprite->getContentSize().width;
 	float ratioY = obRect.size.height / m_MainSprite->getContentSize().height;
 	
 	m_MainSprite->setScaleX(ratioX * needEffectScale);
 	m_MainSprite->setScaleY(ratioY * needEffectScale);
 
+	float addPosX, addPosY;
+	//이게 이펙트마다 anchorPoint가 들어오지 않았을 때 기본으로 어느위치에 존재할 것인가가 다르다. 
+	//앵커포인트 default 값을 -1로 받을 것이기 때문에 0보다 작으면 내가 지정한 위치에 그려진다. 
+	if (AnchorPoint.x < 0 || AnchorPoint.y < 0)
+	{
+		//LinearMissileCollision같은 경우는 미사일 앞에서 중간에 그려지게 하고 시푸다
+		addPosX = obRect.origin.x + 0.25f * obRect.size.width;
+		addPosY = obRect.origin.y + 0.25f * obRect.size.height;
+	}
+	else
+	{
+		addPosX = obRect.origin.x + (AnchorPoint.x - 0.5) * obRect.size.width;
+		addPosY = obRect.origin.y + (AnchorPoint.y - 0.5) * obRect.size.height;
+	}
+	
+	m_MainSprite->setPosition(addPosX, addPosY);
 	m_MainSprite->runAction(Repeat::create(Animate::create(m_MainAnimation), effectPlayNum));
 
 	return;
