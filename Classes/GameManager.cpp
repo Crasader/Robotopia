77f304d429268d6441f9fdc85dc83fb66d316d6a
jwvg0425 <jwvg0425@naver.com>
2014-10-08 21:56:40 +0900

    createGameManager
    createGameManager

---
+++ Classes/GameManager.cpp
@@ -0,0 +1,25 @@
+﻿#include "GameManager.h"
+
+USING_NS_CC;
+
+GameManager* GameManager::m_pInstance = nullptr;
+
+GameManager* GameManager::getInstance()
+{
+	if (m_pInstance == nullptr)
+	{
+		m_pInstance = new GameManager();
+	}
+}
+
+void GameManager::releaseInstance()
+{
+	if (m_pInstance)
+	{
+		delete m_pInstance;
+
+		m_pInstance = nullptr;
+	}
+}
+
+
---
+++ Classes/GameManager.h
@@ -0,0 +1,12 @@
+﻿#include "cocos2d.h"
+
+class GameManager
+{
+public:
+	GameManager* getInstance();
+	void releaseInstance();
+private:
+	static GameManager* m_pInstance;
+	GameManager();
+	~GameManager();
+};
