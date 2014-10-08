0e36ff41d01373f615e14db22533d77b97df587c
jwvg0425 <jwvg0425@naver.com>
2014-10-08 22:26:53 +0900

    manager
    managerCommit

--- Classes/GameManager.h
+++ Classes/GameManager.h
@@ -1,4 +1,15 @@
-﻿#include "cocos2d.h"
+﻿// 
+// GameManager.h
+//
+// 작성자 : 남현욱
+//
+//게임의 모든 매니저 클래스를 포함하고 있는 클래스.
+//매니저 필요하면 얘 하나만 인클루드하면 다 됨.
+#pragma once
+#include "cocos2d.h"
+#include "InputManager.h"
+#include "ResourceManager.h"
+
 
 class GameManager
 {
@@ -7,6 +18,7 @@ public:
 	void releaseInstance();
 private:
 	static GameManager* m_pInstance;
+	static InputManager* m_pInputManager;
 	GameManager();
 	~GameManager();
 };
