2572b0cc7e608cc8b68efe7dbe940e1d216f93ab
Jaewoo Woo <gewoose@gmail.com>
2014-10-09 12:01:20 +0900

    RM_Animation

--- Classes/ResourceManager.cpp
+++ Classes/ResourceManager.cpp
@@ -35,18 +35,25 @@ cocos2d::Animation* ResourceManager::createAnimation(AnimationType type, float d
 		return createAnimation("player_jump%d.png", 1, 1, 0.1f);
 	case AT_PLAYER_ATTACK:
 		return createAnimation("player_attack%d.png", 1, 5, 0.05f);
+	case AT_PLAYER_LANDING:
+		return nullptr;
 	case AT_RUSHMONSTER_STAND:
 		return createAnimation("rushMonster_stand%d.png", 1, 4, 0.1f);
 	case AT_RUSHMONSTER_MOVE:
 		return createAnimation("rushMonster_move%d.png", 1, 4, 0.1f);
 	case AT_RUSHMONSTER_RUSH:
 		return createAnimation("rushMonster_rush%d.png", 1, 4, 0.1f);
-	case AT_LINEAR_MISSILE:
+	case AT_LINEARMISSILE_FLYING:
 		return createAnimation("LinearMissile%d.png", 0, 10, 0.1f);
-	case AT_AIMING_MISSILE:
+	case AT_LINEARMISSILE_COLLISION:
+		return createAnimation("ETLinearMissileCollision%d.png", 1, 9, 0.1f);
+	case AT_AIMINGMISSILE_FLYING:
 		return createAnimation("AimingMissile%d.png", 1, 8, 0.1f);
+	case AT_AIMINGMISSILE_COLLISION:
+		return nullptr;
 	case AT_GATEWAY_OPEN:
 		return createAnimation("Gateway%d.png", 0, 11, 0.2f);
+	
 	}
 	return nullptr;
 }
@@ -55,4 +62,9 @@ cocos2d::Sprite* ResourceManager::createSprite(const char* spriteName)
 {
 	auto sprite = Sprite::createWithSpriteFrameName(spriteName);
 	return sprite;
+}
+
+cocos2d::Sprite* ResourceManager::createSprite(SpriteType type)
+{
+
 }
