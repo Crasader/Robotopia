19b89f9e1d4680e8ed264bc4acdfc10611645ac8
jwvg0425 <jwvg0425@naver.com>
2014-10-09 12:09:01 +0900

    commit
    commit

--- Classes/DataManager.cpp
+++ Classes/DataManager.cpp
@@ -4,13 +4,17 @@ USING_NS_CC;
 
 bool DataManager::init()
 {
-
+	initModuleData();
 	return true;
 }
 
 bool DataManager::initModuleData()
 {
-	CCLOG("%s",FileUtils::getInstance()->getSearchPaths()[0].c_str());
+	unsigned long fileSize;
+	Data data = FileUtils::getInstance()->getDataFromFile(MODULES_FILE_NAME);
+	unsigned char* rawData = data.getBytes();
+
+	delete rawData;
 	return true;
 }
 
--- Classes/DataManager.h
+++ Classes/DataManager.h
@@ -1,11 +1,33 @@
 #pragma once
 #include "cocos2d.h"
 #include "Utils.h"
+#define MODULES_FILE_NAME "Data\\Stage\\moduleList.mld"
+
+struct StageModuleData
+{
+	int width, height;
+	std::vector<int> data;
+};
+
+struct StageData
+{
+	int width, height;
+	std::vector<int> data;
+};
+
+struct FloorData
+{
+	int width, height;
+	std::vector<int> data;
+};
 
 class DataManager
 {
 public:
 	bool init();
+	bool initModuleData();
+	DataManager();
+	~DataManager();
 
 private:	
 
