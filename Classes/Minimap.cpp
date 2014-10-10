7b06128ad5c096b0620faa38b470240c12596daa
Jaewoo Woo <gewoose@gmail.com>
2014-10-10 15:59:40 +0900

    UI_minimapCurrent

--- Classes/Minimap.cpp
+++ Classes/Minimap.cpp
@@ -34,8 +34,24 @@ bool Minimap::init()
 	{
 		for (int i = 0; i < fd.width; ++i)
 		{
-			int room = GET_STAGE_MANAGER()->getFloorDataByIdx(i, j);
-			if (room != 0)
+			int roomNum = GET_STAGE_MANAGER()->getFloorDataByIdx(i, j);
+			int currentRoom = GET_STAGE_MANAGER()->getCurStageNum();
+			if (currentRoom == roomNum)
+			{
+				auto currentRoomRect = DrawNode::create();
+				Vec2 points[] =
+				{
+					Vec2(0, MINIMAPSCALE),
+					Vec2(MINIMAPSCALE, MINIMAPSCALE),
+					Vec2(MINIMAPSCALE, 0),
+					Vec2(0, 0),
+				};
+				currentRoomRect->drawPolygon(points, 4, Color4F(0.9f, 0.4f, 0.1f, 1), 0, Color4F(0.2f, 0.7f, 0.3f, 1));
+				currentRoomRect->setAnchorPoint(Point(0, 0));
+				currentRoomRect->setPosition(Point(i * MINIMAPSCALE, j * MINIMAPSCALE));
+				minimapBg->addChild(currentRoomRect);
+			}
+			else if (roomNum != 0)
 			{
 				auto roomRect = DrawNode::create();
 				Vec2 points[] =
