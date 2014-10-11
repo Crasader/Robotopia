9bbd4e6f33736d954848be974a1a99e3531e8715
jwvg0425 <jwvg0425@naver.com>
2014-10-12 01:07:15 +0900

    bat2
    bat2

--- Classes/BatMonster.cpp
+++ Classes/BatMonster.cpp
@@ -1,1 +1,36 @@
-﻿
+﻿#include "BatMonster.h"
+#include"GameManager.h"
+
+
+USING_NS_CC;
+
+bool BatMonster::init()
+{
+	if (!Monster::init())
+	{
+		return false;
+	}
+
+	m_Type = OT_BAT_MONSTER;
+
+	m_Animations[BM_FIND] = GET_RESOURCE_MANAGER()->createAnimation(AT_MONSTER_BAT);
+	m_Animations[BM_ATTACK] = GET_RESOURCE_MANAGER()->createAnimation(AT_MONSTER_BAT);
+
+
+	m_MainSprite = Sprite::create();
+	m_MainSprite->setScale(2);
+
+	this->addChild(m_MainSprite);
+	m_MoveSpeed = 30;
+	m_IsOnGravity = true;
+
+	m_Info.maxHp = 100;
+	m_Info.hp = m_Info.maxHp;
+
+	return true;
+}
+
+void BatMonster::collisionOccured(InteractiveObject* enemy, Directions dir)
+{
+
+}
--- Classes/BatMonster.h
+++ Classes/BatMonster.h
@@ -1,1 +1,21 @@
-﻿
+﻿#include "Monster.h"
+
+
+class BatMonster : public Monster
+{
+public:
+	OVERRIDE bool init();
+
+	OVERRIDE void collisionOccured(InteractiveObject* enemy, Directions dir);
+
+	void update(float dTime);
+
+	CREATE_FUNC(BatMonster);
+private:
+	enum State
+	{
+		BM_FIND,
+		BM_ATTACK,
+		BM_STATE_NUM,
+	};
+};
