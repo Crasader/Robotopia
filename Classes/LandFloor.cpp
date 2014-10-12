22e704af758e09ea73789cb1e8302a2fef6d7955
ozt88 <kl1467@gmail.com>
2014-10-12 12:10:02 +0900

    fix

--- Classes/LandFloor.cpp
+++ Classes/LandFloor.cpp
@@ -9,13 +9,18 @@ bool LandFloor::init()
 	{
 		return false;
 	}
-	m_MainSprite = GET_RESOURCE_MANAGER()->createSprite( "floor.png" );
-	this->setAnchorPoint( Point::ZERO );
+	m_MainSprite = Sprite::create();
 	this->addChild( m_MainSprite);
+	m_PieceSprite = GET_RESOURCE_MANAGER()->createSprite( "FloorPiece.png" );
+	float bufferX = (m_PieceSprite->getContentSize().width - m_MainSprite->getContentSize().width)* 0.5;
+	float bufferY = ( m_PieceSprite->getContentSize().height - m_MainSprite->getContentSize().height )*0.5;
+	m_PieceSprite->setPosition( bufferX , bufferY );
+	this->addChild( m_PieceSprite);
 
 	m_Type = OT_FLOOR;
-	m_Width = m_MainSprite->getContentSize().width;
-	m_Height = m_MainSprite->getContentSize().height;
+	m_Width = 32;
+	m_Height = 32;
+
 	m_IsOverlapable = true;
 
 	return true;
